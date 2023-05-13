c45d6471484d8547c3a355b1d721b034b2adf1b7
Kyle Gilliam <kgilliam125@gmail.com>
2023-05-12 20:48:33 -0700

    Prettify

--- src/components/CopyableFact.tsx
+++ src/components/CopyableFact.tsx
@@ -1,17 +1,12 @@
 import { CopyIcon } from "@chakra-ui/icons";
-import {
-  Button,
-  HStack,
-  Text,
-  useClipboard,
-} from "@chakra-ui/react";
+import { Button, HStack, Text, useClipboard } from "@chakra-ui/react";
 
-export const CopyableFact = ({fact}: any) => {
+export const CopyableFact = ({ fact }: any) => {
   const { onCopy } = useClipboard(fact);
   return (
-    <HStack bg="factContainerBackground" p={'2'} rounded={"xl"} mb="1rem">
+    <HStack bg="factContainerBackground" p={"2"} rounded={"xl"} mb="1rem">
       <Text>{fact}</Text>
-      <Button onClick={onCopy} bg='button' color='accentText'>
+      <Button onClick={onCopy} bg="button" color="accentText">
         <CopyIcon />
       </Button>
     </HStack>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import {
-  Link as ChakraLink,
   Text,
-  Code,
   List,
-  ListIcon,
   ListItem,
-  useClipboard,
-  HStack,
-  Button,
-  UnorderedList,
 } from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon, CopyIcon } from "@chakra-ui/icons";
-
 import { Hero } from "../components/Hero";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
@@ -22,24 +13,29 @@ import { facts } from "../data/facts";
 import { CopyableFact } from "../components/CopyableFact";
 
 const Index: NextPageWithLayout = () => {
-
   return (
     <Container height="100vh">
       <Hero />
       <Main>
-        <Text color="accentText" fontSize={"xl"} align={"center" } mt="-1rem" mb="2rem">Copy those facts and fight the FUD</Text>
+        <Text
+          color="accentText"
+          fontSize={"xl"}
+          align={"center"}
+          mt="-1rem"
+          mb="2rem"
+        >
+          Copy those facts and fight the FUD
+        </Text>
 
-        <UnorderedList spacing={3} my={0} color="text">
-          {
-            facts.map((fact, index) => {
-              return (
-                <ListItem key={index}>
-                  <CopyableFact fact={fact} />
-                </ListItem>
-              )
-            })
-          }
-        </UnorderedList>
+        <List spacing={3} my={0} color="text">
+          {facts.map((fact, index) => {
+            return (
+              <ListItem key={index}>
+                <CopyableFact fact={fact} />
+              </ListItem>
+            );
+          })}
+        </List>
       </Main>
     </Container>
   );
